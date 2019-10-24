199b2b134b041e8a7199869a45bc936345002ac0
Alvin Sim <sim.alvin@gmail.com>
2019-10-24 15:58:18 +1000

    Updated Favourites to allow delete selected comic

--- src/Components/Comic/Favourites.jsx
+++ src/Components/Comic/Favourites.jsx
@@ -1,22 +1,40 @@
-import { Card, Empty, List } from 'antd';
+import { Button, Card, Empty, Icon, List } from 'antd';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import { removeFavouriteTrigger } from './actions';
 import { getFavouriteComics } from './selectors';
 
 const Favourites = (props) => {
   const { favourites } = props;
 
-  const renderedComic = (comic) => (
-    <List.Item>
-      <Card title={`${comic.title} - ${comic.date}`}>
-        <div className="comic">
-          <img alt={comic.alt} src={comic.img} title={comic.alt} />
-        </div>
-        <div className="transcript">{comic.transcript}</div>
-      </Card>
-    </List.Item>
-  );
+  const renderedComic = (comic) => {
+    const onClickDelete = () => {
+      const { removeFavourite } = props;
+      removeFavourite(comic.comicId);
+    };
+
+    const buttonDelete = (
+      <Button onClick={onClickDelete}>
+        <Icon type="delete" />
+      </Button>
+    );
+
+
+    return (
+      <List.Item>
+        <Card
+          extra={buttonDelete}
+          title={`${comic.title} - ${comic.date}`}
+        >
+          <div className="comic">
+            <img alt={comic.alt} src={comic.img} title={comic.alt} />
+          </div>
+          <div className="transcript">{comic.transcript}</div>
+        </Card>
+      </List.Item>
+    )
+  };
 
   const gridProps = {
     gutter: 16,
@@ -51,6 +69,7 @@ const Favourites = (props) => {
 
 Favourites.propTypes = {
   favourites: PropTypes.instanceOf(Array),
+  removeFavourite: PropTypes.func.isRequired,
 };
 
 Favourites.defaultProps = {
@@ -61,4 +80,8 @@ const mapStateToProps = state => ({
   favourites: getFavouriteComics(state),
 });
 
-export default connect(mapStateToProps)(Favourites);
+const mapDispatchToProps = dispatch => ({
+  removeFavourite: comicId => dispatch(removeFavouriteTrigger(comicId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favourites);
